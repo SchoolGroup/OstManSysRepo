2bfc7bf564b30cc3efa28b785a4fade43652b192
AudriusSa <audriusd3@gmail.com>
2017-05-04 11:16:12 +0200

    LoginChanges
    Redirecting page after login

--- OstManSysMVVM/Handler/LogInHandler.cs
+++ OstManSysMVVM/Handler/LogInHandler.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using OstManSysMVVM.Model;
 using OstManSysMVVM.Persistency;
+using OstManSysMVVM.View;
 using OstManSysMVVM.ViewModel;
 
 namespace OstManSysMVVM.Handler
@@ -32,7 +35,20 @@ namespace OstManSysMVVM.Handler
                     if (data1.ElementAt(s).Equals(ResidentViewModel.Account.Password))
                     {
                        ResidentViewModel.CurrentResident = new PersistencyFacade().GetResident(ResidentViewModel.Account);
-
+                        if (ResidentViewModel.CurrentResident.Type == "Resident")
+                        {
+                            var newFrame = new Frame();
+                            newFrame.Navigate(typeof(ResidentView));
+                            Window.Current.Content = newFrame;
+                            Window.Current.Activate();
+                        }
+                        else if (ResidentViewModel.CurrentResident.Type == "BoardMember")
+                        {
+                            var newFrame = new Frame();
+                            newFrame.Navigate(typeof(BoardMemberView));
+                            Window.Current.Content = newFrame;
+                            Window.Current.Activate();
+                        }
 
                     }
                 }
