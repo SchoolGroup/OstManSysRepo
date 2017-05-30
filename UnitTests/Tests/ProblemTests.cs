062ba93ac6d8955387752d65d9b30a5c00ada9ea
Aleksandar Varchev <aleksvarchev@gmail.com>
2017-05-30 22:44:26 +0200

    Unit Tests

---
+++ UnitTests/Tests/ProblemTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using OstManSysMVVM.Model;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ProblemTests
+    {
+        [TestMethod]
+        public void ProblemDescriptionTest()
+        {
+            Problem newProblem = new Problem();
+            newProblem.Header = "Header";
+            newProblem.Description = "Description";
+            newProblem.Note = "Note";
+            string testString = newProblem.Header + newProblem.Description + newProblem.Note;
+            Assert.AreEqual(testString, "HeaderDescriptionNote");
+        }
+
+        [TestMethod]
+        public void ProblemApartmentIDTest()
+        {
+            Problem newProblem = new Problem();
+            newProblem.ApartmentID = 1;
+            Assert.AreEqual(1, newProblem.ApartmentID);
+        }
+
+        [TestMethod]
+        public void ProblemIDTest()
+        {
+            Problem newProblem = new Problem();
+            newProblem.ProblemID = 1;
+            Assert.AreEqual(1, newProblem.ProblemID);
+        }
+    }
+}
